5b7f6431ca844a79d72e6c00321256d9f4f974de
Equinaut <33580646+Equinaut@users.noreply.github.com>
2019-12-23 00:16:32 +0000

    First version
    First version of the code, a few animations are included

---
+++ RGBLEDSnowflake/Animations.ino
@@ -0,0 +1,210 @@
+//Define all modes
+void mode0() { //Blue-green random pixels
+  //Run at start
+  for (int i=0; i<NUMPIXELS;i++) {
+    pixels.setPixelColor(i,pixels.Color(0,random(255),random(255)));
+  }
+  pixels.show();
+  boolean modeChange = false;
+  //Keep running until escaped
+  while (!modeChange) {
+    pixels.setPixelColor(int(random(0,NUMPIXELS)),pixels.Color(0,random(255),random(255)));
+    pixels.show();
+    delay(8);
+    modeChange = changeMode();
+  }
+}
+void mode1() { //Random wipe
+  int dirs[] = {2,5,3,4};
+  for (int i=0;i<4;i++) {
+    wipe(random(255),random(255),random(255),15,dirs[i]);
+    if (changeMode()) return;
+  }
+}
+void mode2() {
+  for (int col=0;col<6;col++) {
+    for (int i=0;i<91;i++) {
+      pixels.setPixelColor(spiral[i],mainCols[col]);
+      pixels.show();
+      delay(10);
+      if (changeMode()) return;
+    }
+  }
+}
+void mode3() {
+  for (int col=0;col<6;col+=2) {
+    wipe(0,0,0,100,0,false,false,mainCols[col]);
+    wipe(0,0,0,100,1,false,false,mainCols[col+1]);
+    if (changeMode()) break;
+  }
+}
+void mode4() {
+  for (int col=0;col<6;col+=2) {
+    wipe(0,0,0,35,2,false,false,mainCols[col]);
+    wipe(0,0,0,35,3,false,false,mainCols[col+1]);
+    if (changeMode()) return;
+  }
+}void mode5() {
+  for (int col=0;col<6;col+=2) {
+    wipe(0,0,0,35,4,false,false,mainCols[col]);
+    wipe(0,0,0,35,5,false,false,mainCols[col+1]);
+    if (changeMode()) return;
+  }
+}void mode6() {
+  for (int col=0;col<6;col+=2) {
+    wipe(0,0,0,100,6,false,false,mainCols[col]);
+    wipe(0,0,0,100,7,false,false,mainCols[col+1]);
+    if (changeMode()) return;
+  }
+}
+
+
+/*
+void mode0() {
+  for (int i=0; i<91; i++) {
+      pixels.setPixelColor(i,pixels.Color((100*i/91),(100-100*i/91),0));
+      pixels.show();  
+      delay(10);
+  }
+  for (int i=90; i>=0; i--) {
+      pixels.setPixelColor(i,pixels.Color(0,0,0));
+      pixels.show();  
+      delay(10);
+  }
+}
+
+void mode1() {
+  for (int i=0; i<91; i++) {
+      pixels.setPixelColor(i,pixels.Color((100*i/91),0,(100-100*i/91)));
+      pixels.show();  
+      delay(15);
+  }
+  delay(1000);
+  for (int i=90; i>=0; i--) {
+      pixels.setPixelColor(i,pixels.Color((100*i/91),(100-100*i/91),0));
+      pixels.show();  
+      delay(15);
+  }
+  delay(1000);
+}
+
+void mode2() {
+  int dist = 100;
+  wipe(255,0,0,dist,0);
+  wipe(0,255,0,dist,1);
+  wipe(0,0,255,dist,0);
+  wipe(255,0,0,dist,1);
+  wipe(0,255,0,dist,0);
+  wipe(0,0,255,dist,1);  
+}
+void mode3() {
+  int dist = 20;
+  wipe(255,0,0,dist,2);
+  wipe(0,255,0,dist,3);
+  wipe(0,0,255,dist,2);
+  wipe(255,0,0,dist,3);
+  wipe(0,255,0,dist,2);
+  wipe(0,0,255,dist,3);  
+}
+void mode4() {
+  int dist = 30;
+  wipe(255,0,0,dist,4);
+  wipe(0,255,0,dist,5);
+  wipe(0,0,255,dist,4);
+  wipe(255,0,0,dist,5);
+  wipe(0,255,0,dist,4);
+  wipe(0,0,255,dist,5);  
+}
+void mode5() {
+  int dist = 30;
+  wipe(255,0,0,dist,2);
+  wipe(0,255,0,dist,4);
+  wipe(0,0,255,dist,3);
+  wipe(255,0,0,dist,5);
+  wipe(0,255,0,dist,0);
+  wipe(0,0,255,dist,1);
+}
+void mode6() {
+  int dist = 150;
+  wipe(255,0,0,dist,6);
+  wipe(0,255,0,dist,6);
+  wipe(0,0,255,dist,6);
+  wipe(255,0,0,dist,6);
+  wipe(0,255,0,dist,6);
+  wipe(0,0,255,dist,6);
+}
+void mode7() {
+  int r = 255;int g=0;int b=0;int dist = 50;
+  int r2 = 0;int g2 = 255; int b2 = 0;
+  for (int row=0; row<27; row++) {
+    int invrow = 26-row;
+    for (int i=0;i<91;i++) {
+      if (rows[i]==row && (wings[i]==0 || wings[i]==3 || wings[i]==6)) {pixels.setPixelColor(i,pixels.Color(r,g,b));}
+      if (rows[i]==invrow && !(wings[i]==0 || wings[i]==3 || wings[i]==6)) {pixels.setPixelColor(i,pixels.Color(r2,g2,b2));}
+    } 
+    delay(dist); pixels.show();
+  }
+  pixels.clear();
+  //else if (dir==3) {for (int row=26; row>=0; row--) {for (int i=0;i<91;i++) {if (rows[i]==row) {pixels.setPixelColor(i,pixels.Color(r,g,b));}} delay(dist); pixels.show();}}
+}
+void mode8() {
+  int dist = 100;
+  split(255,0,0,dist,2);
+  split(0,255,0,dist,3);  
+}
+
+void mode9() {
+    int r = 255;
+    int g = 0;
+    int b = 0;
+    int stage = 0;
+    int diff=5;
+    while (stage<=5) {
+        if (stage==0) {g+=diff; if (g>=255) {g=255; stage+=1;}}
+        else if (stage==1) {r-=diff; if (r<=0) {r=0; stage+=1;}}
+        else if (stage==2) {b+=diff; if (b>=255) {b=255; stage+=1;}}
+        else if (stage==3) {g-=diff; if (g<=0) {g=0; stage+=1;}}
+        else if (stage==4) {r+=diff; if (r>=255) {r=255; stage+=1;}}
+        else if (stage==5) {b-=diff; if (b<=0) {b=0; stage+=1;}}
+        for (int i=0; i<91; i++) {pixels.setPixelColor(i,pixels.Color(r,g,b));}
+        pixels.show();
+      }
+}
+void mode10() {
+    int r[] = {255,223,191,159,127,95,63,31};
+    int g[] = {0,0,0,0,0,0,0,0};
+    int b[] = {0,0,0,0,0,0,0,0};
+    int stage[] = {0,0,0,0,0,0,0,0};
+    int diff=20;
+    while (stage[7]<=5) {
+        for (int j=0; j<8; j++) {
+          if (stage[j]==0) {g[j]+=diff; if (g[j]>=255) {g[j]=255; stage[j]+=1;}}
+          else if (stage[j]==1) {r[j]-=diff; if (r[j]<=0) {r[j]=0; stage[j]+=1;}}
+          else if (stage[j]==2) {b[j]+=diff; if (b[j]>=255) {b[j]=255; stage[j]+=1;}}
+          else if (stage[j]==3) {g[j]-=diff; if (g[j]<=0) {g[j]=0; stage[j]+=1;}}
+          else if (stage[j]==4) {r[j]+=diff; if (r[j]>=255) {r[j]=255; stage[j]+=1;}}
+          else if (stage[j]==5) {b[j]-=diff; if (b[j]<=0) {r[j]=255;g[j]=0;b[j]=0; stage[j]+=1;}}
+          for (int i=0; i<91; i++) {if (rings[i]==j) {pixels.setPixelColor(i,pixels.Color(r[j],g[j],b[j]));}}
+          pixels.show();
+        }
+      }
+}
+void mode11() {
+    int r[] = {255,246,237,228,219,210,201,192,183,174,165,156,147,138,129,120,111,102,93,84,75,66,57,48,39,30,21};
+    int g[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int b[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int stage[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int diff=40;
+    while (stage[26]<=5) {
+        for (int j=0; j<27; j++) {
+          if (stage[j]==0) {g[j]+=diff; if (g[j]>=255) {g[j]=255; stage[j]+=1;}}
+          else if (stage[j]==1) {r[j]-=diff; if (r[j]<=0) {r[j]=0; stage[j]+=1;}}
+          else if (stage[j]==2) {b[j]+=diff; if (b[j]>=255) {b[j]=255; stage[j]+=1;}}
+          else if (stage[j]==3) {g[j]-=diff; if (g[j]<=0) {g[j]=0; stage[j]+=1;}}
+          else if (stage[j]==4) {r[j]+=diff; if (r[j]>=255) {r[j]=255; stage[j]+=1;}}
+          else if (stage[j]==5) {b[j]-=diff; if (b[j]<=0) {r[j]=255;g[j]=0;b[j]=0; stage[j]+=1;}}
+          for (int i=0; i<91; i++) {if (rows[i]==j) {pixels.setPixelColor(i,pixels.Color(r[j],g[j],b[j]));}}
+          pixels.show();
+        }
+      }
+}*/
--- RGBLEDSnowflake/RGBLEDSnowflake.ino
+++ RGBLEDSnowflake/RGBLEDSnowflake.ino
@@ -1,1 +1,104 @@
+#include <Adafruit_NeoPixel.h>
 
+#define TOTALMODES 7
+#define NUMPIXELS 91
+#define LEDPIN 6
+#define BUTTONPIN 5
+int mode = 0;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_RGB + NEO_KHZ400);
+typedef uint32_t color;
+color mainCols[] = {pixels.Color(150,0,0),pixels.Color(150,150,0),pixels.Color(0,150,0),pixels.Color(0,150,150),pixels.Color(0,0,150),pixels.Color(150,0,150)}; //List of basic colours to be used
+
+//Define LED Groups
+//There are 7 wings including the middle
+const int wings[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6};
+//There are 8 rings
+//OLD RINGS const int rings[] = {5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 6, 6, 6, 6, 6, 6, 7};
+const int rings[] = {2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 1, 1, 1, 1, 1, 1, 0};
+//There are 27 rows
+const int rows[] = {18, 19, 21, 22, 21, 24, 25, 25, 26, 24, 25, 21, 22, 23, 15, 16, 17, 16, 15, 17, 16, 19, 20, 20, 21, 20, 19, 18, 11, 10, 9, 7, 6, 6, 4, 7, 6, 9, 10, 11, 10, 8, 8, 6, 5, 4, 5, 2, 1, 1, 0, 2, 1, 5, 4, 3, 11, 10, 9, 7, 6, 6, 4, 7, 6, 9, 10, 11, 10, 8, 15, 16, 17, 19, 20, 20, 21, 19, 20, 17, 16, 15, 16, 18, 14, 15, 14, 12, 11, 12, 13};
+//There are 17 columns
+const int cols[] = {8, 8, 8, 9, 10, 9, 10, 8, 8, 7, 6, 6, 7, 8, 11, 12, 13, 14, 14, 15, 16, 15, 16, 14, 14, 12, 13, 14, 11, 12, 13, 13, 12, 14, 14, 15, 16, 15, 16, 14, 14, 14, 8, 8, 8, 7, 6, 7, 6, 8, 8, 9, 10, 10, 9, 8, 5, 4, 3, 3, 4, 2, 2, 1, 0, 1, 0, 2, 2, 2, 5, 4, 3, 3, 4, 2, 2, 1, 0, 1, 0, 2, 2, 2, 7, 8, 9, 9, 8, 7, 8};
+//List of led numbers in order of a spiral
+const int spiral[] = {90,88,87,86,85,84,89,42,28,14,0,70,56,43,29,15,1,71,57,44,30,16,2,72,58,69,59,60,46,45,55,54,53,32,31,41,40,39,18,17,27,26,25,4,3,13,12,11,74,73,83,82,81,67,68,65,63,61,47,49,51,33,35,37,19,21,23,5,7,9,75,77,79,66,64,62,48,50,52,34,36,38,20,22,24,6,8,10,76,78,80};
+
+void setup() {
+  pinMode(BUTTONPIN,INPUT);
+  pixels.setBrightness(50);
+  pixels.begin();
+}
+
+void loop() {
+  changeMode();
+  typedef void (*function) (void);
+  function modes[] = {mode0,mode1,mode2,mode3,mode4,mode5,mode6};    //An array of all the modes
+  modes[mode]();
+  pixels.show();
+} 
+
+boolean changeMode() {  //Checks if the button is pressed, an if it is it moves on to hte next mode
+  if (digitalRead(BUTTONPIN)==HIGH) {
+    mode+=1;
+    if(mode>=TOTALMODES) mode=0;
+    
+    pixels.clear();
+    for (int i=0; i<=mode;i++) pixels.setPixelColor(i,pixels.Color(255,255,255));
+    pixels.show();
+    
+    delay(500);
+    
+    pixels.clear();
+    pixels.show();
+    return true;
+  }
+  return false;
+}
+
+//Simple animations
+
+void wipe(int r=0,int g=0,int b=0,int dist = 100,int dir=0,boolean hsv=false,boolean useRGB=true,color col=pixels.Color(0,0,0)) {
+  int startLayer,endLayer,layerStep;
+  int *groups;
+  
+  switch (dir) {
+    case 0: startLayer = 0,  endLayer = 8,  layerStep = 1;  groups = rings;  break;   //Wipes from outside to inside
+    case 1: startLayer = 7,  endLayer = 0,  layerStep = -1; groups = rings;  break;   //Wipes from inside to outside
+    case 2: startLayer = 0,  endLayer = 27, layerStep = 1;  groups = rows;   break;   //Wipes from top to bottom
+    case 3: startLayer = 26, endLayer = 0,  layerStep = -1; groups = rows;   break;   //Wipes from bottom to top
+    case 4: startLayer = 0,  endLayer = 17, layerStep = 1;  groups = cols;   break;   //Wipes from left to right
+    case 5: startLayer = 16, endLayer = 0,  layerStep = -1; groups = cols;   break;   //Wipes from right to left
+    case 6: startLayer = 5,  endLayer = 0,  layerStep = -1; groups = wings;  break;   //Wipes through each wing clockwise
+    case 7: startLayer = 6,  endLayer = 0,  layerStep = 1;  groups = wings;  break;   //Wipes through each wing counter-clockwise
+  }
+  if (useRGB && !hsv) col = pixels.Color(r,g,b);
+  else if (useRGB && hsv) col = pixels.ColorHSV(r,g,b);
+  
+  for (int layer=startLayer; layer<max(startLayer+1,endLayer) && layer>=0; layer+=layerStep) {
+    for (int i=0;i<=NUMPIXELS;i++) if (groups[i]==layer) pixels.setPixelColor(i,col);
+    if (changeMode()) return;
+    delay(dist); 
+    pixels.show();
+  }
+}
+
+void split(int r,int g,int b,int dist = 100, int dir=0, boolean hsv=false,boolean useRGB = true,color col=pixels.Color(0,0,0)) {
+  int startLayer,endLayer,layerStep,totalLayers;
+  int *groups;
+  switch (dir) {
+    case 0: startLayer = 0,  endLayer = 13, layerStep =  1, totalLayers=26; groups = rows; break; //Wipes inwards  vertically 
+    case 1: startLayer = 13, endLayer = 0,  layerStep = -1, totalLayers=26; groups = rows; break; //Wipes outwards vertically
+    case 2: startLayer = 0,  endLayer = 8,  layerStep =  1, totalLayers=16; groups = cols; break; //Wipes inwards  horizontally
+    case 3: startLayer = 8,  endLayer = 0,  layerStep = -1, totalLayers=16; groups = cols; break; //Wipes outwards horizontally
+  }
+  
+  if (useRGB && !hsv) col = pixels.Color(r,g,b);
+  else if (useRGB && hsv) col = pixels.ColorHSV(r,g,b);
+  
+  for (int layer=startLayer; layer<max(startLayer+1,endLayer+1) && layer>=0; layer+=layerStep) {
+    for (int i=0;i<91;i++) if (groups[i]==layer || groups[i]==totalLayers-layer) pixels.setPixelColor(i,col);
+    if (changeMode()) return;
+    delay(dist);
+    pixels.show();
+  }
+}
