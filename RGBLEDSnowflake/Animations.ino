c606703a56dcb632205eb75040714d8d16acec89
Equinaut <33580646+Equinaut@users.noreply.github.com>
2019-12-23 22:02:15 +0000

    A few more modes

--- RGBLEDSnowflake/Animations.ino
+++ RGBLEDSnowflake/Animations.ino
@@ -1,4 +1,38 @@
-//Define all modes
+//Simple animations
+
+void wipe(color col=pixels.Color(0,0,0),int wipeSpeed=4,int dir=0) {
+  for (int i=-400;i<400;i+=wipeSpeed) {
+    for (int pixel=0;pixel<NUMPIXELS;pixel++) {
+      switch (dir) {
+        case 0: if (ys[pixel]<=i)    pixels.setPixelColor(pixel,col); break;   //Wipes from top to botom
+        case 1: if (ys[pixel]>=-1*i) pixels.setPixelColor(pixel,col); break;   //Wipes from bottom to top
+        case 2: if (xs[pixel]<=i)    pixels.setPixelColor(pixel,col); break;   //Wipes from left to right
+        case 3: if (xs[pixel]>=-1*i) pixels.setPixelColor(pixel,col); break;   //Wipes from right to left
+      }
+    }
+    pixels.show();
+    if (changeMode()) return;
+  }
+}
+void circleFrom(color col=pixels.Color(0,0,0),int radStep=5,int centerLED = 90,int dir=0) { //Circles in or out from a particular LED on the snowflake
+  int maxDistance = 0;
+  int distances[NUMPIXELS];
+  
+  for (int i=0; i<NUMPIXELS;i++) {
+    distances[i] = pow(pow(xs[i]-xs[centerLED],2)+pow(ys[i]-ys[centerLED],2),0.5 );
+    if (distances[i]>maxDistance) maxDistance = distances[i];
+  }
+  for (int radius = 0;radius<=maxDistance+radStep;radius+=radStep) {
+    if (dir==0) for (int i=0; i<NUMPIXELS;i++) if (distances[i]<=radius) pixels.setPixelColor(i,col);             //Circle in
+    if (dir==1) for (int i=0; i<NUMPIXELS;i++) if (distances[i]>=maxDistance-radius) pixels.setPixelColor(i,col); //Circle out 
+    pixels.show();
+    if (changeMode()) return;  
+  }
+}
+
+
+//All modes
+
 void mode0() { //Blue-green random pixels
   //Run at start
   for (int i=0; i<NUMPIXELS;i++) {
@@ -17,7 +51,7 @@ void mode0() { //Blue-green random pixels
 void mode1() { //Random wipe
   int dirs[] = {0,3,1,2};
   for (int i=0;i<4;i++) {
-    wipe(pixels.Color(random(255),random(255),random(255)),15,dirs[i]);
+    wipe(pixels.Color(random(255),random(255),random(255)),4,dirs[i]);
     if (changeMode()) return;
   }
 }
@@ -34,34 +68,39 @@ void mode2() {
 void mode3() {
   for (int col=0;col<6;col+=2) {
     circleFrom(mainCols[col],5,90,0);
+    if (changeMode()) return;
     circleFrom(mainCols[col+1],5,90,1);
-    if (changeMode()) break;
+    if (changeMode()) return;
   }
 }
 void mode4() {
   for (int col=0;col<6;col+=2) {
-    wipe(mainCols[col],35,0);
-    wipe(mainCols[col],35,1);
+    wipe(mainCols[col],4,0);
+    if (changeMode()) return;
+    wipe(mainCols[col+1],4,1);
     if (changeMode()) return;
   }
 }
 void mode5() {
   for (int col=0;col<6;col+=2) {
-    wipe(mainCols[col],35,2);
-    wipe(mainCols[col+1],35,3);
+    wipe(mainCols[col],4,2);
+    if (changeMode()) return;
+    wipe(mainCols[col+1],4,3);
     if (changeMode()) return;
   }
 }
 void mode6() {
-  for (int col=0;col<6;col+=2) {
-    wipe(mainCols[col],100,4);
-    wipe(mainCols[col+1],100,5);
-    if (changeMode()) return;
-  }
-}
-void mode7() {
   for (int col=0;col<6;col++) {
     circleFrom(mainCols[col],5,int(random(90)));
     if (changeMode()) return;
   }
+}
+void mode7() {
+  for (int pixel=0;pixel<NUMPIXELS;pixel++) {
+    int r = int(float(xs[pixel]+400)*255/800);
+    int g = int(float(ys[pixel]+400)*255/800);
+    
+    pixels.setPixelColor(pixel,pixels.Color(r,g,0));
+  }
+  pixels.show();
 }
--- RGBLEDSnowflake/RGBLEDSnowflake.ino
+++ RGBLEDSnowflake/RGBLEDSnowflake.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 
 #define TOTALMODES 8
+
 #define NUMPIXELS 91
 #define LEDPIN 6
 #define BUTTONPIN 5
@@ -11,17 +12,6 @@ typedef void (*function) (void);
 typedef uint32_t color;
 color mainCols[] = {pixels.Color(150,0,0),pixels.Color(150,150,0),pixels.Color(0,150,0),pixels.Color(0,150,150),pixels.Color(0,0,150),pixels.Color(150,0,150)}; //List of basic colours to be used
 
-//Define LED Groups
-//There are 7 wings including the middle
-const int wings[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6};
-//There are 8 rings
-//OLD RINGS const int rings[] = {5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 5, 4, 3, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 2, 6, 6, 6, 6, 6, 6, 7};
-//const int rings[] = {2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 2, 3, 4, 5, 5, 6, 7, 6, 7, 6, 7, 5, 5, 5, 1, 1, 1, 1, 1, 1, 0};
-//There are 27 rows
-const int rows[] = {18, 19, 21, 22, 21, 24, 25, 25, 26, 24, 25, 21, 22, 23, 15, 16, 17, 16, 15, 17, 16, 19, 20, 20, 21, 20, 19, 18, 11, 10, 9, 7, 6, 6, 4, 7, 6, 9, 10, 11, 10, 8, 8, 6, 5, 4, 5, 2, 1, 1, 0, 2, 1, 5, 4, 3, 11, 10, 9, 7, 6, 6, 4, 7, 6, 9, 10, 11, 10, 8, 15, 16, 17, 19, 20, 20, 21, 19, 20, 17, 16, 15, 16, 18, 14, 15, 14, 12, 11, 12, 13};
-//There are 17 columns
-const int cols[] = {8, 8, 8, 9, 10, 9, 10, 8, 8, 7, 6, 6, 7, 8, 11, 12, 13, 14, 14, 15, 16, 15, 16, 14, 14, 12, 13, 14, 11, 12, 13, 13, 12, 14, 14, 15, 16, 15, 16, 14, 14, 14, 8, 8, 8, 7, 6, 7, 6, 8, 8, 9, 10, 10, 9, 8, 5, 4, 3, 3, 4, 2, 2, 1, 0, 1, 0, 2, 2, 2, 5, 4, 3, 3, 4, 2, 2, 1, 0, 1, 0, 2, 2, 2, 7, 8, 9, 9, 8, 7, 8};
-//List of led numbers in order of a spiral from the center out
 const int spiral[] = {90,88,87,86,85,84,89,42,28,14,0,70,56,43,29,15,1,71,57,44,30,16,2,72,58,69,59,60,46,45,55,54,53,32,31,41,40,39,18,17,27,26,25,4,3,13,12,11,74,73,83,82,81,67,68,65,63,61,47,49,51,33,35,37,19,21,23,5,7,9,75,77,79,66,64,62,48,50,52,34,36,38,20,22,24,6,8,10,76,78,80};
 
 const int xs[] = {0, 0, 0, 39, 80, 39, 78, 0, 0, -39, -78, -79, -37, 0, 111, 153, 192, 231, 229, 270, 312, 271, 312, 229, 230, 150, 193, 231, 111, 151, 189, 192, 153, 231, 229, 270, 311, 268, 311, 231, 230, 231, 0, 0, 0, -39, -79, -41, -77, -1, -1, 39, 78, 78, 39, 0, -111, -153, -191, -190, -151, -231, -231, -270, -312, -270, -310, -232, -232, -229, -112, -152, -192, -192, -151, -231, -231, -270, -312, -271, -310, -230, -229, -233, -51, 0, 50, 52, 0, -51, 0};
@@ -37,6 +27,7 @@ void setup() {
 void loop() {
   changeMode();
   function modes[] = {mode0,mode1,mode2,mode3,mode4,mode5,mode6,mode7};    //An array of all the modes
+  
   modes[mode]();
   pixels.show();
 } 
@@ -53,59 +44,4 @@ boolean changeMode() {  //Checks if the button is pressed, an if it is it moves 
     return true;
   }
   return false;
-}
-
-//Simple animations
-
-void wipe(color col=pixels.Color(0,0,0),int dist = 100,int dir=0) {
-  int startLayer,endLayer,layerStep;
-  int *groups;
-  
-  switch (dir) {
-    case 0: startLayer = 0,  endLayer = 27, layerStep = 1;  groups = rows;   break;   //Wipes from top to bottom
-    case 1: startLayer = 26, endLayer = 0,  layerStep = -1; groups = rows;   break;   //Wipes from bottom to top
-    case 2: startLayer = 0,  endLayer = 17, layerStep = 1;  groups = cols;   break;   //Wipes from left to right
-    case 3: startLayer = 16, endLayer = 0,  layerStep = -1; groups = cols;   break;   //Wipes from right to left
-    case 4: startLayer = 5,  endLayer = 0,  layerStep = -1; groups = wings;  break;   //Wipes through each wing clockwise
-    case 5: startLayer = 6,  endLayer = 0,  layerStep = 1;  groups = wings;  break;   //Wipes through each wing counter-clockwise
-  }
-  
-  for (int layer=startLayer; layer<max(startLayer+1,endLayer) && layer>=0; layer+=layerStep) {
-    for (int i=0;i<=NUMPIXELS;i++) if (groups[i]==layer) pixels.setPixelColor(i,col);
-    if (changeMode()) return;
-    delay(dist); 
-    pixels.show();
-  }
-}
-
-void split(color col=pixels.Color(0,0,0),int dist = 100, int dir=0) {
-  int startLayer,endLayer,layerStep,totalLayers;
-  int *groups;
-  switch (dir) {
-    case 0: startLayer = 0,  endLayer = 13, layerStep =  1, totalLayers=26; groups = rows; break; //Wipes inwards  vertically 
-    case 1: startLayer = 13, endLayer = 0,  layerStep = -1, totalLayers=26; groups = rows; break; //Wipes outwards vertically
-    case 2: startLayer = 0,  endLayer = 8,  layerStep =  1, totalLayers=16; groups = cols; break; //Wipes inwards  horizontally
-    case 3: startLayer = 8,  endLayer = 0,  layerStep = -1, totalLayers=16; groups = cols; break; //Wipes outwards horizontally
-  }
-  for (int layer=startLayer; layer<max(startLayer+1,endLayer+1) && layer>=0; layer+=layerStep) {
-    for (int i=0;i<91;i++) if (groups[i]==layer || groups[i]==totalLayers-layer) pixels.setPixelColor(i,col);
-    if (changeMode()) return;
-    delay(dist);
-    pixels.show();
-  }
-}
-void circleFrom(color col=pixels.Color(0,0,0),int radStep=5,int centerLED = 90,int dir=0) { //Circles in or out from a particular LED on the snowflake
-  int maxDistance = 0;
-  int distances[NUMPIXELS];
-  
-  for (int i=0; i<NUMPIXELS;i++) {
-    distances[i] = pow(pow(xs[i]-xs[centerLED],2)+pow(ys[i]-ys[centerLED],2),0.5 );
-    if (distances[i]>maxDistance) maxDistance = distances[i];
-  }
-  for (int radius = 0;radius<=maxDistance+radStep;radius+=radStep) {
-    if (dir==0) for (int i=0; i<NUMPIXELS;i++) if (distances[i]<=radius) pixels.setPixelColor(i,col);             //Circle in
-    if (dir==1) for (int i=0; i<NUMPIXELS;i++) if (distances[i]>=maxDistance-radius) pixels.setPixelColor(i,col); //Circle out 
-    pixels.show();
-    if (changeMode()) return;  
-  }
 }
