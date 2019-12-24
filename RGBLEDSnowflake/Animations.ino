de79d3985d6bbe02d76c891a63a4a352f869fb8e
Equinaut <33580646+Equinaut@users.noreply.github.com>
2019-12-24 12:34:33 +0000

    Added a rotate effect
    Added an effect where a specific colour can rotae round the snowflake.

--- RGBLEDSnowflake/Animations.ino
+++ RGBLEDSnowflake/Animations.ino
@@ -1,20 +1,20 @@
 //Simple animations
 
-void wipe(color col=pixels.Color(0,0,0),int wipeSpeed=4,int dir=0) {
+void wipe(colour col=pixels.Color(0,0,0),int wipeSpeed=4,int dir=0) {
   for (int i=-400;i<400;i+=wipeSpeed) {
     for (int pixel=0;pixel<NUMPIXELS;pixel++) {
       switch (dir) {
-        case 0: if (ys[pixel]<=i)    pixels.setPixelColor(pixel,col); break;   //Wipes from top to botom
-        case 1: if (ys[pixel]>=-1*i) pixels.setPixelColor(pixel,col); break;   //Wipes from bottom to top
-        case 2: if (xs[pixel]<=i)    pixels.setPixelColor(pixel,col); break;   //Wipes from left to right
-        case 3: if (xs[pixel]>=-1*i) pixels.setPixelColor(pixel,col); break;   //Wipes from right to left
+        case 0: if (ys[pixel]<=i)     pixels.setPixelColor(pixel,col); break;  //Wipes from top to botom
+        case 1: if (ys[pixel]>=-1*i)  pixels.setPixelColor(pixel,col); break;  //Wipes from bottom to top
+        case 2: if (xs[pixel]<=i)     pixels.setPixelColor(pixel,col); break;  //Wipes from left to right
+        case 3: if (xs[pixel]>=-1*i)  pixels.setPixelColor(pixel,col); break;  //Wipes from right to left
       }
     }
     pixels.show();
     if (changeMode()) return;
   }
 }
-void circleFrom(color col=pixels.Color(0,0,0),int radStep=5,int centerLED = 90,int dir=0) { //Circles in or out from a particular LED on the snowflake
+void circleFrom(colour col=pixels.Color(0,0,0),int radStep=5,int centerLED = 90,int dir=0) { //Circles in or out from a particular LED on the snowflake
   int maxDistance = 0;
   int distances[NUMPIXELS];
   
@@ -29,7 +29,24 @@ void circleFrom(color col=pixels.Color(0,0,0),int radStep=5,int centerLED = 90,i
     if (changeMode()) return;  
   }
 }
-
+void rotate(colour col=pixels.Color(0,0,0),float rotateSpeed=0.15,int firstSpeed = 30,int radius = 400) {
+  //Stage0 - Run up the first bar of the rotate
+  for (int pixel:spokes[0]) {
+    if (ys[pixel]<-radius) break;
+    pixels.setPixelColor(pixel,col); 
+    pixels.show();
+    delay(firstSpeed);
+    if (changeMode()) return;
+  }
+  for (float a=-PI/2+rotateSpeed;a<=3*PI/2;a+=rotateSpeed) {
+    for (int pixel=0;pixel<NUMPIXELS;pixel++) {
+      if (xs[pixel]>=0 && ys[pixel]<=xs[pixel]*tan(a)  && pow(xs[pixel],2)+pow(ys[pixel],2)<=pow(radius,2)) pixels.setPixelColor(pixel,col);
+      if (ys[pixel]>=xs[pixel]*tan(a) && a>=PI/2 && pow(xs[pixel],2)+pow(ys[pixel],2)<=pow(radius,2)) pixels.setPixelColor(pixel,col);
+    }
+    if (changeMode()) return;
+    pixels.show();  
+  }
+}
 
 //All modes
 
@@ -48,54 +65,39 @@ void mode0() { //Blue-green random pixels
     modeChange = changeMode();
   }
 }
-void mode1() { //Random wipe
-  int dirs[] = {0,3,1,2};
-  for (int i=0;i<4;i++) {
-    wipe(pixels.Color(random(255),random(255),random(255)),4,dirs[i]);
-    if (changeMode()) return;
-  }
-}
-void mode2() {
-  for (int col=0;col<6;col++) {
+
+void mode1() {
+  for (colour col:mainCols) {
     for (int i=0;i<91;i++) {
-      pixels.setPixelColor(spiral[i],mainCols[col]);
+      pixels.setPixelColor(spiral[i],col);
       pixels.show();
       delay(10);
       if (changeMode()) return;
     }
   }
 }
+void mode2() {
+  int dir=0;
+  for (colour col:mainCols) {
+    if (changeMode()) return;
+    circleFrom(col,5,90,dir=1-dir);
+  }
+}
 void mode3() {
-  for (int col=0;col<6;col+=2) {
-    circleFrom(mainCols[col],5,90,0);
+  int dir=0;
+  for (colour col:mainCols) {
     if (changeMode()) return;
-    circleFrom(mainCols[col+1],5,90,1);
-    if (changeMode()) return;
+    wipe(col,4,dir++);
+    if (dir>=4) dir=0;
   }
 }
 void mode4() {
-  for (int col=0;col<6;col+=2) {
-    wipe(mainCols[col],4,0);
-    if (changeMode()) return;
-    wipe(mainCols[col+1],4,1);
+  for (colour col:mainCols) {
+    circleFrom(col,5,int(random(90)));
     if (changeMode()) return;
   }
 }
 void mode5() {
-  for (int col=0;col<6;col+=2) {
-    wipe(mainCols[col],4,2);
-    if (changeMode()) return;
-    wipe(mainCols[col+1],4,3);
-    if (changeMode()) return;
-  }
-}
-void mode6() {
-  for (int col=0;col<6;col++) {
-    circleFrom(mainCols[col],5,int(random(90)));
-    if (changeMode()) return;
-  }
-}
-void mode7() {
   for (int pixel=0;pixel<NUMPIXELS;pixel++) {
     int r = int(float(xs[pixel]+400)*255/800);
     int g = int(float(ys[pixel]+400)*255/800);
@@ -103,4 +105,27 @@ void mode7() {
     pixels.setPixelColor(pixel,pixels.Color(r,g,0));
   }
   pixels.show();
+}
+void mode6() {
+  for (colour col:mainCols) {
+    rotate(col);
+    if (changeMode()) return;
+  }
+}
+void mode7() {
+  int delayVal = 75;
+  for (colour col:mainCols) {
+    for (int i=0;i<8;i++) {
+      for (int j=0;j<6;j++) pixels.setPixelColor(spokes[j][i],col);
+      pixels.show();
+      delay(delayVal);
+      if (changeMode()) return;
+    } 
+    for (int i=0;i<10;i++) {
+      for (int j=0;j<6;j++) pixels.setPixelColor(otherLines[j][i],col);
+      pixels.show();
+      delay(delayVal);
+      if (changeMode()) return;
+    } 
+  }
 }
--- RGBLEDSnowflake/RGBLEDSnowflake.ino
+++ RGBLEDSnowflake/RGBLEDSnowflake.ino
@@ -6,20 +6,35 @@
 #include <Adafruit_NeoPixel.h>
 
 #define TOTALMODES 8
-
+int mode = 6;
+boolean modeChanged = false;
 #define NUMPIXELS 91
 #define LEDPIN 6
 #define BUTTONPIN 5
-int mode = 7;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_RGB + NEO_KHZ400);
 typedef void (*function) (void);
-typedef uint32_t color;
-color mainCols[] = {pixels.Color(150,0,0),pixels.Color(150,150,0),pixels.Color(0,150,0),pixels.Color(0,150,150),pixels.Color(0,0,150),pixels.Color(150,0,150)}; //List of basic colours to be used
+typedef uint32_t colour;
+#define AMOUNTOFMAINCOLS 6
+colour mainCols[] = {pixels.Color(150,0,0),pixels.Color(150,150,0),pixels.Color(0,150,0),pixels.Color(0,150,150),pixels.Color(0,0,150),pixels.Color(150,0,150)}; //List of basic colours to be used
+
+const int spokes[][8] = {{90,88,42,43,44,55,49,50},
+                        {90,87,28,29,30,41,35,36},
+                        {90,86,14,15,16,27,21,22},
+                        {90,85, 0, 1, 2,13, 7, 8},
+                        {90,84,70,71,72,83,77,78},
+                        {90,89,56,57,58,69,63,64}};
+
+const int otherLines[][10] = {{48,47,55,54,53,32,31,41,37,38},
+                            {34,33,41,40,39,18,17,27,23,24},
+                            {20,19,27,26,25, 4, 3,13, 9,10},
+                            {80,79,83,73,74,11,12,13, 5, 6},
+                            {62,61,69,68,67,81,82,83,75,76},
+                            {52,51,55,45,46,60,59,69,65,66}};
 
 const int spiral[] = {90,88,87,86,85,84,89,42,28,14,0,70,56,43,29,15,1,71,57,44,30,16,2,72,58,69,59,60,46,45,55,54,53,32,31,41,40,39,18,17,27,26,25,4,3,13,12,11,74,73,83,82,81,67,68,65,63,61,47,49,51,33,35,37,19,21,23,5,7,9,75,77,79,66,64,62,48,50,52,34,36,38,20,22,24,6,8,10,76,78,80};
 
-const int xs[] = {0, 0, 0, 39, 80, 39, 78, 0, 0, -39, -78, -79, -37, 0, 111, 153, 192, 231, 229, 270, 312, 271, 312, 229, 230, 150, 193, 231, 111, 151, 189, 192, 153, 231, 229, 270, 311, 268, 311, 231, 230, 231, 0, 0, 0, -39, -79, -41, -77, -1, -1, 39, 78, 78, 39, 0, -111, -153, -191, -190, -151, -231, -231, -270, -312, -270, -310, -232, -232, -229, -112, -152, -192, -192, -151, -231, -231, -270, -312, -271, -310, -230, -229, -233, -51, 0, 50, 52, 0, -51, 0};
+const int xs[] = {0, 0, 0, 39, 80, 39, 78, 0, 0, -39, -78, -79, -37, 0, 111, 153, 192, 231, 229, 270, 312, 271, 312, 229, 230, 150, 193, 231, 111, 151, 189, 192, 153, 231, 229, 270, 311, 268, 311, 231, 230, 231, 0, 0, 0, -39, -79, -39, -77, 0, 0, 39, 78, 78, 39, 0, -111, -153, -191, -190, -151, -231, -231, -270, -312, -270, -310, -232, -232, -229, -112, -152, -192, -192, -151, -231, -231, -270, -312, -271, -310, -230, -229, -233, -51, 0, 50, 52, 0, -51, 0};
 const int ys[] = {129, 175, 221, 242, 219, 290, 310, 310, 359, 289, 312, 221, 243, 265, 64, 88, 110, 89, 43, 111, 88, 157, 181, 179, 224, 177, 156, 132, -65, -86, -110, -156, -178, -177, -223, -157, -178, -110, -87, -41, -86, -133, -128, -175, -220, -242, -220, -289, -312, -312, -359, -289, -314, -220, -243, -265, -63, -87, -109, -155, -178, -178, -225, -156, -179, -111, -86, -40, -87, -133, 66, 89, 110, 156, 178, 176, 223, 158, 179, 111, 88, 42, 88, 133, 29, 61, 29, -29, -61, -30, 0};
 
 
@@ -32,12 +47,13 @@ void setup() {
 void loop() {
   changeMode();
   function modes[] = {mode0,mode1,mode2,mode3,mode4,mode5,mode6,mode7};    //An array of all the modes
-  
+  modeChanged =false;//New mode will now be run
   modes[mode]();
   pixels.show();
 } 
 
 boolean changeMode() {  //Checks if the button is pressed, an if it is it moves on to hte next mode
+  if (modeChanged) return true;
   if (digitalRead(BUTTONPIN)==HIGH) {
     if(++mode>=TOTALMODES) mode=0; //Increments the mode by 1 and if it is then higher than the amount of modes, it is set to 0
     pixels.clear();
@@ -46,6 +62,7 @@ boolean changeMode() {  //Checks if the button is pressed, an if it is it moves 
     delay(500);
     pixels.clear();
     pixels.show();
+    modeChanged = true;
     return true;
   }
   return false;
